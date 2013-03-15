d1829306b3f66ff38796b7c560a48b53d73573ff
Travis J. VanDame <tvandame@leandertechnologies.com>
2013-03-15 12:13:50 -0500

    Updated Comments
    1. Included Copyright and License URL
    2. Included GITHUB repo information
    3. Included Example Usage

--- js/libs/fDataGrid.js
+++ js/libs/fDataGrid.js
@@ -1,16 +1,56 @@
 /*
  * fDataGrid 0.1
  * 
- * GITHUB
- * =========================================================
- * HTTP: https://github.com/tvandame/fDataGrid.git
- * SSH: git@github.com:tvandame/fDataGrid.git
- * GIT Read-Only: git://github.com/tvandame/fDataGrid.git
- *
  * Copyright 2013, Travis J. VanDame
  *
  * Licensed under the MIT license:
  * http://www.opensource.org/licenses/MIT
+ * 
+  * GITHUB Repository Information
+ * =========================================================
+ * HTTP: https://github.com/tvandame/fDataGrid.git
+ * SSH: git@github.com:tvandame/fDataGrid.git
+ * GIT Read-Only: git://github.com/tvandame/fDataGrid.git
+ * 
+ * Example Usage
+ * =========================================================
+     
+    var oDataGrid = new mDataGrid(<container div id>);
+    
+     // Set Table Attributes
+    oDataGrid.setId(<assigned to datagrid table id>);
+    oDataGrid.setName(<assigned to datagrid table name>);
+    oDataGrid.setClass(<assigned to datagrid table class>);
+
+    // Add Data Grid Caption
+    oDataGrid.addCaption(<assigned to datagrid table caption>);
+
+    // Enable Multiple Select Option
+    // This will provide you with a checkbox next to each row
+    oDataGrid_1.optionsMultiSelect.enabled = <true>; // false = no checkboxes / true = checkboxes
+
+    // Add Data Grid Header Items
+    oDataGrid.addHeadCell(<header th cell text>);
+    oDataGrid.addHeadCell(<header th cell text>);
+
+    // Example JSON Data
+    var oData = [ // Array
+	    "jon",  // Head Cell 1 Row Data
+	    "doe", // Head Cell 2 Row Data
+	    { // Object For Row Options
+		"id": "1",
+		"name": "1",
+		"style": "",
+		"value": "friends::1",
+		"checked": true
+	    }
+	]
+
+     // Create Table Body Rows Using JSON Example Data Listed Above
+    oDataGrid.addRow(<json object of row data>);
+    
+    // Display Data Grid In Assigned Container
+    oDataGrid_1.init();
  */
 
 /**
