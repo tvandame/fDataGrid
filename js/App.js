00fa97dc9a5e15562228cacc8e695ae8afb48609
Travis J. VanDame <tvandame@gmail.com>
2014-05-02 00:16:37 -0400

    Initial work on app namespacing.

---
+++ js/App.js
@@ -0,0 +1,30 @@
+/** Top level namespace **/
+var App = App || {};
+
+/**
+ * Parse string namespace automatically generating nested
+ * namespaces.
+ * 
+ * @param ns_string
+ */
+App.namespace = function(ns_string) {
+    var parts = ns_string.split('.'),
+        parent = App,
+        pl, i;
+    
+    if (parts[0] == "fdgApp") {
+        parts = parts.slice(1);
+    }
+    
+    pl = parts.length;
+    
+    for (i = 0; i < pl; i++) {
+        if (typeof parent[parts[i]] == 'undefined') {
+            parent[parts[i]] = {};
+        }
+        
+        parent = parent[parts[i]];
+    }
+    
+    return parent;
+};
